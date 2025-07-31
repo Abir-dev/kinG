ea0563f2271725f2b4bd46be5f6daba86869bf27
Abir-dev <abirlalbanerjee44@gmail.com>
2025-07-31 16:01:46 +0530

    Refactor LaunchpadPage: Comment out the Success Stories section for future review

--- client/pages/LaunchpadPage.tsx
+++ client/pages/LaunchpadPage.tsx
@@ -532,7 +532,7 @@ export default function LaunchpadPage() {
       </section>
 
       {/* Success Stories */}
-      <section id="success-stories" className="py-20 px-4">
+      {/* <section id="success-stories" className="py-20 px-4">
         <div className="max-w-7xl mx-auto">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -587,7 +587,7 @@ export default function LaunchpadPage() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* FAQ Section */}
       <section className="py-20 px-4">
